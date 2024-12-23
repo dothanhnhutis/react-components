c133caecb59bd125ed342e2f0deb332d3d9765de
dothanhnhutis <dothanhnhutis@gmail.com>
2024-12-23 11:32:54 +0700

    smart input integer component 80%

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import React from "react";
 import SmartInputInt from "./components/smart-input-int";
 
 function App() {
+  const [value, setValue] = React.useState<string>("0");
+
+  React.useEffect(() => {
+    console.log(value);
+  }, [value]);
+
   return (
     <div>
       <p>smart input integer</p>
-      <SmartInputInt />
+      <SmartInputInt
+        className="border focus-visible:outline-none"
+        input={value}
+        min={0}
+        max={100}
+        onInputChange={(v) => setValue(v)}
+      />
     </div>
   );
 }
--- src/components/smart-input-int.tsx
+++ src/components/smart-input-int.tsx
@@ -1,17 +1,99 @@
 import React from "react";
 
 type SmartInputIntProps = React.ComponentProps<"input"> & {
-  isNegative?: boolean;
+  allowNegative?: boolean;
   min?: number;
   max?: number;
   disabled?: boolean;
   delay?: number;
-  value?: string;
+  input?: string;
   onInputChange?: (v: string) => void;
 };
 
-const SmartInputInt: React.FunctionComponent<SmartInputIntProps> = (props) => {
-  return <input {...props} />;
+const SmartInputInt: React.FunctionComponent<SmartInputIntProps> = ({
+  input,
+  min,
+  max,
+  delay = 1000,
+  allowNegative = true,
+  onInputChange,
+  className = "focus-visible:outline-none",
+  ...props
+}) => {
+  const [value, setValue] = React.useState<string>(input || "");
+  const [negative, setNegative] = React.useState<boolean>(allowNegative);
+
+  if (min && max && min > max) throw new Error("min > max");
+
+  React.useEffect(() => {
+    if (min && min < 0) {
+      setNegative(true);
+    }
+
+    if (max && max < 0) {
+      setNegative(true);
+    }
+
+    if (max && min && (max < 0 || min < 0)) {
+      setNegative(false);
+    }
+  }, [max, min]);
+  console.log(negative);
+
+  React.useEffect(() => {
+    if (onInputChange) {
+      onInputChange(value);
+    }
+    let timer: NodeJS.Timeout;
+    if (value == "-") {
+      timer = setTimeout(() => {
+        setValue("");
+      }, delay);
+    }
+    return () => clearTimeout(timer);
+  }, [value, onInputChange, delay]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue: string = e.target.value;
+    const regex = negative ? /^-?\d*$/ : /^\d*$/;
+
+    if (inputValue.startsWith("-0")) {
+      setValue("0");
+      return;
+    }
+
+    if (inputValue.startsWith("0") && inputValue.length == 2) {
+      if (inputValue == "0-") {
+        setValue("0");
+      } else {
+        setValue(inputValue.slice(-1));
+      }
+      return;
+    }
+
+    let newValue: string;
+    if (regex.test(inputValue)) {
+      if (min && min > parseInt(inputValue)) {
+        newValue = min.toString();
+      } else if (max && max < parseInt(inputValue)) {
+        newValue = max.toString();
+      } else {
+        newValue = inputValue;
+      }
+    } else {
+      newValue = inputValue.substring(0, inputValue.length - 1);
+    }
+    setValue(newValue);
+  };
+
+  return (
+    <input
+      className={className}
+      {...props}
+      value={value}
+      onChange={handleOnChange}
+    />
+  );
 };
 
 export default SmartInputInt;
